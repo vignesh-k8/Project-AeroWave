257fd01ebe0bcd7bb0dbd1a81bf781a94bf7f810
Vignesh Kalakuntla <vigneshkalakuntla@Vigneshs-MacBook-Air.local>
2026-06-04 13:52:27 +0530

    Dark Mode

---
+++ public/javascript/theme.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn?.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙";
+  }
+});
